b99c6c25375ade9cae9f1d3bb6ac2c7f24fd6b18
mzp <mzpppp@gmail.com>
2009-11-02 18:02:28 +0900

    lemma for weaking

--- Subst.v
+++ Subst.v
@@ -45,6 +45,116 @@ split.
     exact H3.
 Qed.
 
+Lemma add_add :
+  forall (tenv : tenv) (x y : string) (r1 r2 : type),
+    (x <> y -> TEnv.Equal (TEnv.add x r1 (TEnv.add y r2 tenv)) (TEnv.add y r2 (TEnv.add x r1 tenv)))  /\
+    (x =  y -> TEnv.Equal (TEnv.add x r1 (TEnv.add y r2 tenv)) (TEnv.add x r1 tenv)).
+Proof.
+split.
+ intros.
+ eapply TEnvWF.Equal_mapsto_iff.
+ split.
+  intros.
+  apply TEnvWF.add_mapsto_iff in H0.
+  decompose [or] H0.
+   inversion H1.
+   rewrite H2 in |- *.
+   rewrite H3 in |- *.
+   rewrite H2 in H.
+   apply TEnv.add_2.
+    auto.
+
+    apply TEnv.add_1.
+    reflexivity.
+
+   decompose [and] H1.
+   apply TEnvWF.add_mapsto_iff in H3.
+   decompose [or] H3.
+    inversion H4.
+    rewrite H5 in |- *; rewrite H6 in |- *.
+    apply TEnv.add_1.
+    reflexivity.
+
+    decompose [and] H4.
+    apply TEnv.add_2.
+     exact H5.
+
+     apply TEnv.add_2.
+      exact H2.
+
+      exact H6.
+
+  intros.
+  apply TEnvWF.add_mapsto_iff in H0.
+  decompose [or] H0.
+   inversion H1.
+   rewrite H2 in |- *; rewrite H3 in |- *.
+   rewrite H2 in H.
+   apply TEnv.add_2.
+    exact H.
+
+    apply TEnv.add_1.
+    reflexivity.
+
+   inversion H1.
+   apply TEnvWF.add_mapsto_iff in H3.
+   decompose [or] H3.
+    inversion H4.
+    rewrite H6 in |- *.
+    rewrite H5 in |- *.
+    apply TEnv.add_1.
+    reflexivity.
+
+    decompose [and] H4.
+    apply TEnv.add_2.
+     exact H5.
+
+     apply TEnv.add_2.
+      exact H2.
+
+      exact H6.
+
+ intros.
+ rewrite H in |- *.
+ eapply TEnvWF.Equal_mapsto_iff.
+ split.
+  intros.
+  apply TEnvWF.add_mapsto_iff in H0.
+  decompose [or] H0.
+   decompose [and] H1.
+   rewrite H3 in |- *.
+   rewrite H2 in |- *.
+   apply TEnv.add_1.
+   reflexivity.
+
+   decompose [and] H1.
+   apply TEnv.add_2.
+    exact H2.
+
+    apply TEnv.add_3 in H3.
+     exact H3.
+
+     exact H2.
+
+  intros.
+  apply TEnvWF.add_mapsto_iff in H0.
+  decompose [or] H0.
+   inversion H1.
+   rewrite H2 in |- *.
+   rewrite H3 in |- *.
+   apply TEnv.add_1.
+   reflexivity.
+
+   decompose [and] H1.
+   apply TEnv.add_2.
+    exact H2.
+
+    apply TEnv.add_2.
+     exact H2.
+
+     exact H3.
+Qed.
+
 Lemma permutation:
   forall (t : term) (tenv1 tenv2 : tenv) (r : type),
     TEnv.Equal tenv1 tenv2 ->
@@ -102,3 +212,4 @@ induction t.
 
    exact H8.
 Qed.
+
