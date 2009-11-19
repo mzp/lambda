02d6ea17a81a3977deca4fbcfaf4469d868b8c8e
mzp <mzpppp@gmail.com>
2009-11-19 22:58:08 +0900

    proof alpha preserv

--- AlphaFact.v
+++ AlphaFact.v
@@ -54,7 +54,7 @@ induction t
 
    exact H7.
 
- (* If *)
+ (* Apply *)
  intros.
  inversion H1.
  apply TApply with (a := a).
@@ -117,12 +117,13 @@ induction t
 
    exact H9.
 Qed.
-(*
+
 Theorem alpha_preserve : forall t tenv x y T S,
   Typed t tenv T -> ~FV y t -> ~BV y t -> TEnv.MapsTo x S tenv ->
      Typed (alpha t x y) (TEnv.add y S tenv) T.
 Proof.
 induction t.
+ (* Var *)
  intros.
  simpl in |- *.
  destruct (string_dec s x).
@@ -148,15 +149,146 @@ induction t.
 
    exact H4.
 
+ (* Bool *)
  intros.
  simpl in |- *.
  inversion H.
  apply TBool.
 
+ (* Lambda *)
  intros.
  simpl in |- *.
  destruct (string_dec s x).
   inversion H.
   apply TLambda.
+  apply permutation with (tenv1 := TEnv.add y S (TEnv.add s t tenv)).
+   apply Equal_add_2.
+   unfold not in |- *; intro; apply H1.
+   rewrite H9 in |- *.
+   apply BVLambda1.
 
-*)
+   apply weaking_FV.
+    unfold not in |- *.
+    intro.
+    apply H0.
+    apply FVLambda.
+     unfold not in |- *; intro; apply H1.
+     rewrite H10 in |- *.
+     apply BVLambda1.
+
+     exact H9.
+
+    unfold not in |- *; intro; apply H1.
+    apply BVLambda2.
+    exact H9.
+
+    apply H8.
+
+  inversion H.
+  apply TLambda.
+  apply permutation with (tenv1 := TEnv.add y S (TEnv.add s t tenv)).
+   apply Equal_add_2.
+   unfold not in |- *.
+   intro.
+   apply H1.
+   rewrite H9 in |- *.
+   apply BVLambda1.
+
+   apply IHt.
+    exact H8.
+
+    unfold not in |- *.
+    intro.
+    apply H0.
+    apply FVLambda.
+     unfold not in |- *; intro; apply H1.
+     rewrite H10 in |- *.
+     apply BVLambda1.
+
+     exact H9.
+
+    unfold not in |- *; intro.
+    apply H1.
+    apply BVLambda2.
+    exact H9.
+
+    apply TEnv.add_2.
+     exact n.
+
+     exact H2.
+
+ (* Apply *)
+ intros.
+ inversion H.
+ simpl in |- *.
+ apply TApply with (a := a).
+  apply IHt1.
+   exact H5.
+
+   unfold not in |- *; intro; apply H0.
+   apply FVApply.
+   left; exact H9.
+
+   unfold not in |- *; intro; apply H1.
+   apply BVApply.
+   left; exact H9.
+
+   exact H2.
+
+  apply IHt2.
+   exact H8.
+
+   unfold not in |- *; intro; apply H0.
+   apply FVApply.
+   right; exact H9.
+
+   unfold not in |- *; intro; apply H1.
+   apply BVApply.
+   right; exact H9.
+
+   exact H2.
+
+ (* If *)
+ intros.
+ simpl in |- *.
+ inversion H.
+ apply TIf.
+  apply IHt1.
+   exact H6.
+
+   unfold not in |- *; intro; apply H0.
+   apply FVIf.
+   left; exact H11.
+
+   unfold not in |- *; intro; apply H1.
+   apply BVIf.
+   left; exact H11.
+
+   exact H2.
+
+  apply IHt2.
+   exact H9.
+
+   unfold not in |- *; intro; apply H0.
+   apply FVIf.
+   right; left; exact H11.
+
+   unfold not in |- *; intro; apply H1.
+   apply BVIf.
+   right; left; exact H11.
+
+   exact H2.
+
+  apply IHt3.
+   exact H10.
+
+   unfold not in |- *; intro; apply H0.
+   apply FVIf.
+   right; right; exact H11.
+
+   unfold not in |- *; intro; apply H1.
+   apply BVIf.
+   right; right; exact H11.
+
+   exact H2.
+Qed.
