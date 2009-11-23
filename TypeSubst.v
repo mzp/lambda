88b7a925adcf64d9e3c668eaf2783b44b4157d7f
mzp <mzpppp@gmail.com>
2009-11-23 19:51:28 +0900

    証明の途中

--- TypeSubst.v
+++ TypeSubst.v
@@ -19,7 +19,8 @@ Definition TEnvSubst (tenv1 tenv2 : tenv) (ctx : ctx):= forall x T S,
 Inductive TermSubst : term -> term -> ctx -> Prop :=
    SVar  : forall ctx x, TermSubst (Var x) (Var x) ctx
  | SBool : forall ctx b, TermSubst (Bool b) (Bool b) ctx
- | SLambda : forall ctx x T S t, TypeSubst T S ctx -> TermSubst (Lambda x T t) (Lambda x S t) ctx
+ | SLambda : forall ctx x T S t s, TypeSubst T S ctx -> TermSubst t s ctx ->
+              TermSubst (Lambda x T t) (Lambda x S s) ctx
  | SApply : forall ctx t1 t2 s1 s2,       TermSubst t1 s1 ctx -> TermSubst t2 s2 ctx ->
               TermSubst (Apply t1 t2) (Apply s1 s2) ctx
  | SIf    : forall ctx t1 t2 t3 s1 s2 s3, TermSubst t1 s1 ctx -> TermSubst t2 s2 ctx -> TermSubst t3 s3 ctx ->
@@ -78,11 +79,12 @@ induction T.
 Qed.
 
 Theorem subst_preserve : forall ctx s tenv2 S t tenv1 T,
-  Typed t tenv1 T -> TEnvSubst tenv1 tenv2 ctx -> TermSubst t s ctx -> TypeSubst T S ctx ->
-  Typed s tenv2 S.
+  Typed t tenv1 T -> (TEnvSubst tenv1 tenv2 ctx -> TermSubst t s ctx -> TypeSubst T S ctx ->
+  Typed s tenv2 S).
 Proof.
 intros until T.
 intro.
+generalize ctx0, s, tenv2, S.
 pattern t, tenv1, T in |- *.
 apply Typed_ind.
  intros.
@@ -95,8 +97,23 @@ apply Typed_ind.
   exact H3.
 
  intros.
- inversion H2.
  inversion H1.
+ inversion H2.
  apply TBool.
 
  intros.
+ inversion H3.
+ inversion H4.
+ assert (S1 = S2).
+  apply subst_uniq with (ctx := ctx1) (T := a).
+   exact H10.
+
+   exact H13.
+
+  rewrite H17 in |- *.
+  apply TLambda.
+  apply H1 with (ctx0 := ctx1).
+   unfold TEnvSubst in |- *.
+   unfold TEnvSubst in H2.
+   intros.
+   apply TEnvWF.add_mapsto_iff in H18.
