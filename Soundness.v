aa1d8e312dae014276e52df4f947706c017c0968
mzp <mzpppp@gmail.com>
2009-11-29 10:25:19 +0900

    Lambdaのsoundnessを示そうとしてる

--- Soundness.v
+++ Soundness.v
@@ -25,6 +25,31 @@ split.
   exact H0.
 Qed.
 
+Lemma lambda_intro : forall T1 T2 S1 tsubst tenv x t,
+  TypeSubst T1 S1 tsubst ->
+  Solution tsubst T2 (TEnv.add x T1 tenv) t ->
+  Solution tsubst (FunT S1 T2) tenv (Lambda x T1 t).
+Proof.
+unfold Solution in |- *.
+intros.
+inversion H2.
+assert (S = S1).
+ apply subst_uniq with (T := T1) (tsubst := tsubst).
+  exact H8.
+
+  exact H.
+
+ rewrite H10 in |- *.
+ apply TLambda.
+ apply H0.
+  apply subst_add.
+   exact H1.
+
+   exact H.
+
+  exact H9.
+Qed.
+
 
 Theorem soundness : forall tenv t T S C tsubst,
   TypeConstraint t tenv S nil C ->
@@ -33,6 +58,7 @@ Theorem soundness : forall tenv t T S C tsubst,
 Proof.
 intros until tsubst.
 intro.
+generalize T.
 pattern t, tenv, S, (nil:tvars), C in |- *.
 apply TypeConstraint_ind.
  intros.
@@ -42,28 +68,17 @@ apply TypeConstraint_ind.
   subst_preserve
    with (tsubst := tsubst) (t := Var s) (tenv1 := tenv0) (T := T0).
   apply TVar.
-  exact H0.
+  trivial.
 
-  exact H2.
+  trivial.
 
-  exact H3.
+  trivial.
 
   unfold Constraint.Solution in H1.
   inversion H1.
   inversion H5.
-  exact H7.
+  trivial.
 
-(*intros.
-unfold Solution in |- *.
-intros.
-apply
- subst_preserve
-  with (tsubst := tsubst) (t := Var s) (tenv1 := tenv0) (T := T0).
- apply TVar.
- exact H0.
-
- exact H2.
-
- exact H3.
-
-*)
+ intros.
+ eapply lambda_solution in H2.
+  apply H1 in H2.
