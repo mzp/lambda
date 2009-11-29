6b68a7334c2dae6b214851d83ffdc792aa22ef53
mzp <mzpppp@gmail.com>
2009-11-30 08:47:09 +0900

    補題を追加

--- Constraint.v
+++ Constraint.v
@@ -1,39 +1,41 @@
 Require Import List.
+Require Import Sets.Ensembles.
 Require Import String.
-Require Import Typing.
 
+Require Import Typing.
 Require Import Term.
 Require Import TypeSubst.
 
-Definition tconst := list (type * type).
+
+Definition tconst := Ensemble (type * type).
+Definition InConst x xs     := Ensembles.In (type*type) xs x.
+Definition UnionConst xs ys := Ensembles.Union (type*type) xs ys.
+Definition AddConst x xs    := Ensembles.Add (type*type) xs x.
+
 Definition Unified (c : tconst) (t : tsubst) := forall S T U,
-  In (S,T) c -> TypeSubst S U t /\ TypeSubst T U t.
-Definition tvars := list string.
+  InConst (S,T) c -> TypeSubst S U t /\ TypeSubst T U t.
+
+Definition tvars := Ensemble string.
+Definition InTVars x xs := Ensembles.In string xs x.
+Definition UnionTVars xs ys := Ensembles.Union string xs ys.
+Definition AddTVars x xs    := Ensembles.Add string xs x.
+Definition DisjointTVars xs ys := Ensembles.Disjoint string xs ys.
+
 
 Definition FvTConst x c := forall S T,
-  In (S,T) c -> FvT x S \/ FvT x T.
+  InConst (S,T) c -> FvT x S \/ FvT x T.
 
 Definition FvTEnv x tenv := forall y T,
   TEnv.MapsTo y T tenv -> FvT x T.
 
 Definition DisjointFV xs T := forall x,
-  (FvT x T -> ~ In x xs) /\ (In x xs -> ~ FvT x T).
+  (FvT x T -> ~ InTVars x xs) /\ (InTVars x xs -> ~ FvT x T).
 
-Definition Disjoint {A : Type} (xs ys : list A) := forall x,
-  (In x xs -> ~ In x ys) /\ (In x ys -> ~ In x xs).
-
-Lemma Disjoint_sym : forall A (xs ys : list A),
-  Disjoint xs ys -> Disjoint ys xs.
-Proof.
-unfold Disjoint in |- *.
-intros.
-specialize (H x).
-inversion H.
-split; [ apply H1 | apply H0 ].
-Qed.
+(*Definition Disjoint {A : Type} (xs ys : list A) := forall x,
+  (In x xs -> ~ In x ys) /\ (In x ys -> ~ In x xs).*)
 
 Definition Fresh x X1 X2 T1 T2 C1 C2 tenv t1 t2 :=
-  ~ In x X1  /\ ~ In x X2 /\
+  ~ InTVars x X1  /\ ~ InTVars x X2 /\
   ~ FvT x T1 /\ ~ FvT x T2 /\
   ~ FvTConst x C1 /\ ~ FvTConst x C2 /\
   ~ FvTEnv x tenv /\
@@ -51,19 +53,115 @@ Inductive TypeConstraint : term -> tenv -> type -> tvars -> tconst -> Prop :=
 | CTApply : forall x t1 t2 T1 T2 tenv X1 X2 X C1 C2 C,
     TypeConstraint t1 tenv T1 X1 C1 ->
     TypeConstraint t2 tenv T2 X2 C2 ->
-    Disjoint X1 X2 -> DisjointFV X T1 -> DisjointFV X T2 ->
-    X = x :: X1 ++ X2 ->
+    DisjointTVars X1 X2 -> DisjointFV X2 T1 -> DisjointFV X1 T2 ->
+    X = AddTVars x (UnionTVars X1 X2) ->
     Fresh x X1 X2 T1 T2 C1 C2 tenv t1 t2 ->
-    C = (T1,FunT T2 (VarT x)) :: C1 ++ C2 ->
+    C = AddConst (T1,FunT T2 (VarT x)) (UnionConst C1 C2) ->
     TypeConstraint (Apply t1 t2) tenv (VarT x) X C
 | CTIf : forall t1 t2 t3 T1 T2 T3 tenv X1 X2 X3 X C1 C2 C3 C,
     TypeConstraint t1 tenv T1 X1 C1 ->
     TypeConstraint t2 tenv T2 X2 C3 ->
     TypeConstraint t3 tenv T3 X3 C3 ->
-    X = X1 ++ X2 ++ X3 ->
-    Disjoint X1 X2 -> Disjoint X2 X3 -> Disjoint X3 X1 ->
-    C = (T1,BoolT) :: (T2,T3) :: C1 ++ C2 ++ C3 ->
+    X = UnionTVars X1 (UnionTVars X2 X3) ->
+    DisjointTVars X1 X2 -> DisjointTVars X2 X3 -> DisjointTVars X3 X1 ->
+    C = AddConst (T1,BoolT) (AddConst (T2,T3) (UnionConst C1 (UnionConst C2 C3))) ->
     TypeConstraint (If t1 t2 t3) tenv T2 X C.
 
 Definition Solution tsubst T tenv t S C := forall X,
   TypeConstraint t tenv S X C /\ Unified C tsubst /\ TypeSubst S T tsubst.
+
+Lemma Disjoint_union : forall A (X Y Z: Ensemble A),
+  Disjoint A (Union A X Z) Y ->
+  Disjoint A X Y.
+Proof.
+intros.
+inversion H.
+apply Disjoint_intro.
+intro.
+specialize (H0 x).
+intro.
+apply H0.
+inversion H1.
+apply Intersection_intro.
+ apply Union_introl.
+ trivial.
+
+ trivial.
+Qed.
+
+Lemma Disjoint_add : forall A (X Y : Ensemble A) x,
+  Disjoint A (Add A X x) Y -> Disjoint A X Y.
+Proof.
+unfold Add in |- *.
+intros.
+apply Disjoint_union with (Z := Singleton A x).
+trivial.
+Qed.
+
+Lemma Disjoint_sym : forall A (X Y : Ensemble A),
+  Disjoint A X Y -> Disjoint A Y X.
+Proof.
+intros.
+inversion H.
+apply Disjoint_intro.
+intros.
+specialize (H0 x).
+intro.
+apply H0.
+inversion H1.
+apply Intersection_intro.
+ trivial.
+
+ trivial.
+Qed.
+
+Lemma Union_sym : forall A (X Y : Ensemble A),
+  Union A X Y = Union A Y X.
+Proof.
+intros.
+apply Extensionality_Ensembles.
+unfold Same_set in |- *.
+unfold Included in |- *.
+split; intros.
+ inversion H.
+  apply Union_intror.
+  trivial.
+
+  apply Union_introl.
+  trivial.
+
+ inversion H.
+  apply Union_intror.
+  trivial.
+
+  apply Union_introl.
+  trivial.
+Qed.
+
+
+Lemma tvars_preserve: forall t tenv T X1 X2 X C,
+   TypeConstraint t tenv T X1 C ->
+   X = UnionTVars X1 X2 ->
+   DisjointTVars X1 X2 ->
+   DisjointFV X2 T ->
+   TypeConstraint t tenv T X C.
+
+induction t.
+Focus 4.
+ intros.
+ inversion H.
+ apply
+  CTApply
+   with
+     (T1 := T1)
+     (T2 := T2)
+     (C1 := C1)
+     (C2 := C2)
+     (X1 := UnionTVars X0 X2)
+     (X2 := X3).
+  apply IHt1 with (X1 := X0) (X2 := X2).
+   trivial.
+
+   reflexivity.
+
+
