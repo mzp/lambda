440aa8f6f09fa440ffabf34514e558871e0c9ff5
mzp <mzpppp@gmail.com>
2009-12-21 08:17:52 +0900

    健全性の証明を修正

--- Soundness.v
+++ Soundness.v
@@ -6,22 +6,22 @@ Require Import Typing.
 Require Import Constraint.
 Require Import TypeSubst.
 
-Theorem soundness : forall t tenv S X C T tsubst,
-  TypeConstraint t tenv S X C ->
-  Constraint.Solution tsubst T tenv t S C ->
+Theorem soundness : forall t tenv Ts S X C T tsubst,
+  TypeConstraint t tenv Ts S X C ->
+  Constraint.Solution tsubst T tenv Ts t S C ->
   TypeSubst.Solution tsubst T tenv t.
 Proof.
 intros until tsubst.
 intro.
 generalize T.
-pattern t, tenv, S, X, C in |- *.
-apply TypeConstraint_ind; intros; unfold Solution; simpl.
+pattern t, tenv, Ts, S, X, C in |- *.
+apply TypeConstraint_ind; intros; unfold Solution in |- *; simpl in |- *.
  apply var_solution_inv in H1.
  apply TVar.
  trivial.
 
  apply lambda_solution_inv in H2.
- inversion H2.
+ decompose [and] H2.
  apply H1 in H4.
  rewrite H3 in |- *.
  apply TLambda.
@@ -42,44 +42,40 @@ apply TypeConstraint_ind; intros; unfold Solution; simpl.
      (S := T0)
      (C1 := C1)
      (X1 := X1) in H2.
-  inversion H2.
-  inversion H11.
+  decompose [and] H2.
   apply TApply with (a := type_subst T2 tsubst).
    assert (T0 = type_subst (VarT x) tsubst).
-    unfold Constraint.Solution in H9.
-    inversion H9.
-    inversion H14.
-    inversion H16.
+    unfold Constraint.Solution in H11.
+    decompose [ex] H11.
+    decompose [and] H13.
     trivial.
 
-    rewrite H14 in |- *.
+    rewrite H13 in |- *.
     apply H1.
     simpl in |- *.
-    simpl in H10.
-    rewrite <- H10 in |- *.
-    trivial.
+    simpl in H12.
+    rewrite <- H12 in |- *.
+    tauto.
 
    apply H3.
-   trivial.
-
-  trivial.
-
-  rewrite <- H8 in |- *.
-  trivial.
-
- apply (if_solution_inv t1 t2 t3 T0 T1 T2 T3 X1 X2 X3 C1 C2 C3 tenv0 tsubst)
-  in H4.
-  inversion H4.
-  inversion H13.
-  apply TIf; [apply H1 | apply H3 | apply H5]; trivial.
-
-  trivial.
+   tauto.
 
   trivial.
 
   rewrite <- H10 in |- *.
   trivial.
 
+ apply (if_solution_inv t1 t2 t3 T0 T1 T2 T3 X1 X2 X3 C1 C2 C3 _ _ tsubst)
+  in H4.
+  decompose [and] H4.
+  apply TIf; [apply H1 | apply H3 | apply H5]; trivial.
+
+  trivial.
+
+  trivial.
+
+  rewrite <- H15 in |- *.
+  trivial.
+
  trivial.
 Qed.
-
