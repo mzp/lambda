0c77e308adedc03a567ddc315c2a8e16ed845e6c
mzp <mzpppp@gmail.com>
2009-12-24 08:35:21 +0900

    unifyに関する補題

--- Constraint.v
+++ Constraint.v
@@ -86,6 +86,17 @@ Inductive TypeConstraint : term -> tenv -> list type -> type -> tvars -> tconst 
 Definition Solution tsubst T tenv Ts t S C := exists X,
   TypeConstraint t tenv Ts S X C /\ Unified C tsubst /\ T = type_subst S tsubst.
 
+Lemma Unified_Union_intro : forall C1 C2 tsubst,
+  Unified C1 tsubst ->
+  Unified C2 tsubst ->
+  Unified (TConst.union C1 C2) tsubst.
+Proof.
+unfold Unified in |- *.
+intros.
+apply (TConst.WFact.union_iff C1 C2 _) in H1.
+decompose [or] H1; [ apply H in H2 | apply H0 in H2 ]; trivial.
+Qed.
+
 Lemma Unified_Union : forall C1 C2 tsubst,
   Unified (TConst.union C1 C2) tsubst -> Unified C1 tsubst.
 Proof.
@@ -102,6 +113,24 @@ Proof.
 unfold Unified in |- *.
 intros.
 inversion H.
+Qed.
+
+Lemma Unified_Add_intro : forall c C tsubst,
+  Unified C tsubst ->
+  Unified (TConst.FSet.singleton c) tsubst ->
+  Unified (TConst.add c C) tsubst.
+Proof.
+unfold Unified in |- *.
+intros.
+apply (TConst.WFact.add_iff C c _) in H1.
+decompose [or] H1.
+ destruct c.
+ apply TConst.WFact.singleton_iff in H2.
+ apply H0 in H2.
+ trivial.
+
+ apply H in H2.
+ trivial.
 Qed.
 
 Lemma Unified_Add : forall c C tsubst,
