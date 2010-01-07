bd51551cbca5799d6cb3382ba6ddab8a34c2611c
mzp <mzpppp@gmail.com>
2010-01-08 07:44:40 +0900

    余計な型アノテーションを消した

--- Typing.v
+++ Typing.v
@@ -9,15 +9,15 @@ Definition tenv := table type.
 Definition empty := Table.empty type.
 
 Inductive Typed : term -> tenv -> type -> Prop :=
-  | TVar    : forall (tenv : tenv) (s : string) (T : type),
+  | TVar    : forall tenv s T,
                 Table.MapsTo s T tenv -> Typed (Var s) tenv T
-  | TBool   : forall (tenv : tenv) (b : bool) ,
+  | TBool   : forall tenv b,
                 Typed (Bool b) tenv BoolT
-  | TLambda : forall (tenv : tenv) (x : string) (S T : type) (t : term),
+  | TLambda : forall tenv x S T t,
                 Typed t (Table.add x T tenv) S -> Typed (Lambda x T t) tenv (FunT T S)
-  | TApply  : forall (tenv : tenv) (t1 t2 : term) (S T: type),
+  | TApply  : forall tenv t1 t2 S T,
                 Typed t1 tenv (FunT S T) -> Typed t2 tenv S -> Typed (Apply t1 t2) tenv T
-  | TIf     : forall (tenv : tenv) (t1 t2 t3 : term) (T : type),
+  | TIf     : forall tenv t1 t2 t3 T,
                 Typed t1 tenv BoolT -> Typed t2 tenv T -> Typed t3 tenv T ->
                    Typed (If t1 t2 t3) tenv T.
 
