c13529e7789dac4948c8ea5dfea57d7f7a5a9ea9
mzp <mzpppp@gmail.com>
2010-01-13 05:51:19 +0900

    インデントを揃えた

--- theorems/TypingRule.v
+++ theorems/TypingRule.v
@@ -9,15 +9,16 @@ Definition empty := Table.empty type.
 
 Inductive Typed : term -> tenv -> type -> Prop :=
   | TVar    : forall tenv s T,
-                Table.MapsTo s T tenv -> Typed (Var s) tenv T
+     Table.MapsTo s T tenv -> Typed (Var s) tenv T
   | TBool   : forall tenv b,
-                Typed (Bool b) tenv BoolT
+     Typed (Bool b) tenv BoolT
   | TLambda : forall tenv x S T t,
-                Typed t (Table.add x T tenv) S -> Typed (Lambda x T t) tenv (FunT T S)
+     Typed t (Table.add x T tenv) S -> Typed (Lambda x T t) tenv (FunT T S)
   | TApply  : forall tenv t1 t2 S T,
-                Typed t1 tenv (FunT S T) -> Typed t2 tenv S -> Typed (Apply t1 t2) tenv T
+     Typed t1 tenv (FunT S T) -> Typed t2 tenv S ->
+     Typed (Apply t1 t2) tenv T
   | TIf     : forall tenv t1 t2 t3 T,
-                Typed t1 tenv BoolT -> Typed t2 tenv T -> Typed t3 tenv T ->
-                   Typed (If t1 t2 t3) tenv T.
+     Typed t1 tenv BoolT -> Typed t2 tenv T -> Typed t3 tenv T ->
+     Typed (If t1 t2 t3) tenv T.
 
 
