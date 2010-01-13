ac37524199009fddec14e079d0988fd3245f8c30
mzp <mzpppp@gmail.com>
2010-01-14 07:57:15 +0900

    unificationに関する定理を一般化(しようとしてる)

--- theorems/Constraint.v
+++ theorems/Constraint.v
@@ -13,11 +13,45 @@ Module TypePairDec := PairDec TypeDec TypeDec.
 Module TConst := Sets.Make TypePairDec.
 Definition tconst := TConst.t.
 
+Delimit Scope const_scope with tconst.
+Bind Scope const_scope with tconst.
+Open Scope const_scope.
+Infix "|_|" := TConst.union (at level 50) : const_scope.
+Close Scope  const_scope.
+
 Definition FreeC x c := forall S T,
   TConst.In (S,T) c -> FreeT x S \/ FreeT x T.
 
 Definition Unified (c : tconst) (t : tsubst) := forall S T,
   TConst.In (S,T) c -> type_subst S t = type_subst T t.
+
+Lemma unified_union_iff : forall C1 C2 tsubst,
+  (Unified C1 tsubst /\ Unified C2 tsubst) <->
+  Unified (C1 |_| C2) tsubst.
+Proof.
+unfold Unified in |- *.
+split; intros.
+ decompose [and] H.
+ apply TConst.WFact.union_iff in H0.
+ decompose [or] H0;
+  [ apply H1 in H3 | apply H2 in H3 ];
+    tauto.
+
+ split; intros;
+  apply H;
+  apply <- TConst.WFact.union_iff;
+  tauto.
+Qed.
+
+Lemma unified_empty : forall tsubst,
+  Unified TConst.empty tsubst.
+Proof.
+unfold Unified in |- *.
+intros.
+inversion H.
+Qed.
+
+Hint Immediate unified_empty.
 
 Lemma Unified_Union_intro : forall C1 C2 tsubst,
   Unified C1 tsubst ->
