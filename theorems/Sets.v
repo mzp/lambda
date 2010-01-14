740eefab56dbee26018dff3535bb7ab85e792c1d
mzp <mzpppp@gmail.com>
2010-01-14 09:02:29 +0900

    sets

--- theorems/Sets.v
+++ theorems/Sets.v
@@ -1,5 +1,5 @@
-Require Import Ensembles.
 Require Import String.
+Require Import Util.
 Require Import DecidableType.
 Require Import FSetWeakList.
 Require Import FSetFacts.
@@ -20,81 +20,62 @@ Module Make(Dec : DecidableType).
   Definition add   x xs     := FSet.add x xs.
   Definition Disjoint xs ys := forall x, ~ FSet.In x (FSet.inter xs ys).
 
-  Lemma disjoint_union : forall X Y Z,
-    Disjoint (union X Z) Y -> Disjoint X Y.
+  Lemma equal_ind : forall X Y,
+    (forall x, FSet.In x X -> FSet.In x Y) ->
+    (forall x, FSet.In x Y -> FSet.In x X) ->
+    X = Y.
   Proof.
-  unfold Disjoint in |- *.
   intros.
-  specialize (H x).
-  intro.
-  apply H.
-  apply (WFact.inter_iff (union X Z) Y x).
-  apply (WFact.inter_iff X Y x) in H0.
-  inversion H0.
-  split.
-   apply (WFact.union_iff X Z x).
-   left; trivial.
-
-   trivial.
+  apply Extensionality_Set.
+  unfold FSet.Equal in |- *.
+  split; intros; auto.
   Qed.
 
-  Lemma disjoint_add : forall X Y x,
-    Disjoint (add x X) Y -> Disjoint X Y.
+  Lemma add_union : forall x X,
+    add x X = union (FSet.singleton x) X.
   Proof.
-  unfold Disjoint in |- *.
   intros.
-  specialize (H x0).
-  intro.
-  apply H.
-  apply (WFact.inter_iff (add x X) Y _).
-  apply (WFact.inter_iff X Y) in H0.
-  inversion H0.
-  split.
-   apply (WFact.add_iff X x x0).
-   right; trivial.
+  apply equal_ind; intros.
+   apply <-  WFact.union_iff.
+   apply WFact.add_iff in H.
+   decompose [or] H.
+    left.
+    apply <-  WFact.singleton_iff.
+    assumption.
 
-   trivial.
+    tauto.
+
+   apply WFact.union_iff in H.
+   apply <-  WFact.add_iff.
+   decompose [or] H.
+    left.
+    apply WFact.singleton_iff in H0.
+    assumption.
+
+    tauto.
   Qed.
-
-  Lemma disjoint_sym : forall X Y,
-    Disjoint X Y -> Disjoint Y X.
-  Proof.
-  unfold Disjoint in |- *.
-  intros.
-  intro.
-  specialize (H x).
-  apply H.
-  apply (WFact.inter_iff X Y x).
-  apply (WFact.inter_iff Y X x) in H0.
-  inversion H0.
-  split; trivial.
-  Qed.
-
 
   Lemma union_assoc : forall X Y Z,
     union X (union Y Z) = (union (union X Y) Z).
   Proof.
   intros.
-  unfold union in |- *.
-  apply Extensionality_Set.
-  unfold FSet.Equal in |- *.
-  split; intros.
+  apply equal_ind; intros.
    apply WFact.union_iff in H.
-   apply <- WFact.union_iff (* Generic printer *).
+   apply <- WFact.union_iff.
    decompose [or] H.
     left.
-    apply <- WFact.union_iff (* Generic printer *).
+    apply <- WFact.union_iff.
     tauto.
 
     apply WFact.union_iff in H0.
     decompose [or] H0.
      left.
-     apply <- WFact.union_iff (* Generic printer *).
+     apply <- WFact.union_iff.
      tauto.
 
      tauto.
 
-   apply <- WFact.union_iff (* Generic printer *).
+   apply <- WFact.union_iff.
    apply WFact.union_iff in H.
    decompose [or] H.
     apply WFact.union_iff in H0.
@@ -102,11 +83,11 @@ Module Make(Dec : DecidableType).
      tauto.
 
      right.
-     apply <- WFact.union_iff (* Generic printer *).
+     apply <- WFact.union_iff.
      tauto.
 
     right.
-    apply <- WFact.union_iff (* Generic printer *).
+    apply <- WFact.union_iff.
     tauto.
   Qed.
 
@@ -114,35 +95,118 @@ Module Make(Dec : DecidableType).
     union X Y = union Y X.
   Proof.
   intros.
-  apply Extensionality_Set.
-  unfold FSet.Equal in |- *.
-  split.
-   intro.
-   apply (WFact.union_iff Y X a).
-   apply (WFact.union_iff X Y a) in H.
-   inversion H.
-    right; trivial.
+  apply equal_ind; intros.
+   apply <- WFact.union_iff.
+   apply WFact.union_iff in H.
+   decompose [or] H; tauto.
 
-    left; trivial.
-
-   intros.
-   apply (WFact.union_iff X Y a).
-   apply (WFact.union_iff Y X a) in H.
-   inversion H.
-    right; trivial.
-
-    left; trivial.
+   apply <- WFact.union_iff.
+   apply WFact.union_iff in H.
+   decompose [or] H; tauto.
   Qed.
 
-  Lemma disjoint_left : forall x X Y,
-    Disjoint X Y -> In x X -> ~ In x Y.
+  Lemma disjoint_sym : forall X Y,
+    Disjoint X Y -> Disjoint Y X.
   Proof.
   unfold Disjoint in |- *.
   intros.
   specialize (H x).
-  intro; apply H.
-  apply <- WFact.inter_iff (* Generic printer *).
+  Contrapositive H.
+  apply <- WFact.inter_iff.
+  apply WFact.inter_iff in H0.
+  decompose [and] H0.
   tauto.
   Qed.
 
+  Lemma disjoint_union_iff: forall X Y Z,
+    Disjoint (union X Y) Z <-> Disjoint X Z /\ Disjoint Y Z.
+  Proof.
+  unfold Disjoint in |- *.
+  split; intros.
+   split; intros; specialize (H x).
+    Contrapositive H.
+    apply <-  WFact.inter_iff.
+    apply WFact.inter_iff in H0.
+    decompose [and] H0.
+    split; auto.
+    apply <-  WFact.union_iff.
+    tauto.
+
+    Contrapositive H.
+    apply WFact.inter_iff in H0.
+    decompose [and] H0.
+    apply <-  WFact.inter_iff.
+    split; auto.
+    apply <-  WFact.union_iff.
+    tauto.
+
+   decompose [and] H.
+   specialize (H0 x).
+   specialize (H1 x).
+   intro.
+   apply WFact.inter_iff in H2.
+   decompose [and] H2.
+   apply WFact.union_iff in H3.
+   decompose [or] H3.
+    apply H0.
+    apply <-  WFact.inter_iff.
+    tauto.
+
+    apply H1.
+    apply <-  WFact.inter_iff.
+    tauto.
+  Qed.
+
+  Lemma disjoint_add_iff : forall X Y x,
+    Disjoint (add x X) Y <-> ~In x Y /\ Disjoint X Y.
+  Proof.
+  intros.
+  rewrite add_union in |- *.
+  split; intros.
+   apply disjoint_union_iff in H.
+   decompose [and] H.
+   split; auto.
+   unfold Disjoint in H0.
+   specialize (H0 x).
+   Contrapositive H0.
+   apply <- WFact.inter_iff.
+   split; auto.
+   apply <- WFact.singleton_iff.
+   reflexivity.
+
+   apply <- disjoint_union_iff.
+   decompose [and] H.
+   split; auto.
+   unfold Disjoint.
+   unfold Disjoint in H1.
+   intros.
+   specialize (H1 x0).
+   Contrapositive H1.
+   apply WFact.inter_iff in H2.
+   decompose [and] H2.
+   apply WFact.singleton_iff in H3.
+
+
+
+  Qed.
+
+  Lemma disjoint_iff : forall X Y,
+    Disjoint X Y <-> (forall x, (In x X -> ~ In x Y) /\ (In x Y -> ~ In x X)).
+  Proof.
+  unfold Disjoint in |- *.
+  split; intros.
+  specialize (H x).
+  split; intros;
+   Contrapositive H;
+   apply <- WFact.inter_iff;
+   tauto.
+
+  specialize (H x).
+  decompose [and] H.
+  intro.
+  apply WFact.inter_iff in H2.
+  decompose [and] H2.
+  apply H0 in H3.
+  contradiction.
+  Qed.
 End Make.
