9ac958da5064b172ae9d702de01310610c734808
mzp <mzpppp@gmail.com>
2010-01-14 15:46:19 +0900

    Constraintの上の補題を整理した

--- theorems/Constraint.v
+++ theorems/Constraint.v
@@ -16,7 +16,7 @@ Definition tconst := TConst.t.
 Delimit Scope const_scope with tconst.
 Bind Scope const_scope with tconst.
 Open Scope const_scope.
-Infix "|_|" := TConst.union (at level 50) : const_scope.
+Infix "∪" := TConst.union (at level 50) : const_scope.
 Close Scope  const_scope.
 
 Definition FreeC x c := forall S T,
@@ -27,7 +27,7 @@ Definition Unified (c : tconst) (t : tsubst) := forall S T,
 
 Lemma unified_union_iff : forall C1 C2 tsubst,
   (Unified C1 tsubst /\ Unified C2 tsubst) <->
-  Unified (C1 |_| C2) tsubst.
+  Unified (C1 ∪ C2) tsubst.
 Proof.
 unfold Unified in |- *.
 split; intros.
@@ -43,6 +43,31 @@ split; intros.
   tauto.
 Qed.
 
+Lemma unified_add_iff : forall S T C tsubst,
+  Unified (TConst.add (S,T) C) tsubst <-> (type_subst S tsubst = type_subst T tsubst /\ Unified C tsubst).
+Proof.
+intros.
+rewrite TConst.add_union.
+split; intros.
+ apply unified_union_iff in H.
+ decompose [and] H.
+ split; auto.
+ apply H0.
+ apply <- TConst.WFact.singleton_iff.
+ tauto.
+
+ decompose [and] H.
+ apply (unified_union_iff (TConst.FSet.singleton (S,T)) C _).
+ split; auto.
+ unfold Unified.
+ intros.
+ apply TConst.WFact.singleton_iff in H2.
+ simpl in H2.
+ decompose [and] H2.
+ rewrite <- H3, <- H4.
+ assumption.
+Qed.
+
 Lemma unified_empty : forall tsubst,
   Unified TConst.empty tsubst.
 Proof.
@@ -52,61 +77,3 @@ inversion H.
 Qed.
 
 Hint Immediate unified_empty.
-
-Lemma Unified_Union_intro : forall C1 C2 tsubst,
-  Unified C1 tsubst ->
-  Unified C2 tsubst ->
-  Unified (TConst.union C1 C2) tsubst.
-Proof.
-unfold Unified in |- *.
-intros.
-apply (TConst.WFact.union_iff C1 C2 _) in H1.
-decompose [or] H1; [ apply H in H2 | apply H0 in H2 ]; trivial.
-Qed.
-
-Lemma Unified_Union : forall C1 C2 tsubst,
-  Unified (TConst.union C1 C2) tsubst -> Unified C1 tsubst.
-Proof.
-unfold Unified in |- *.
-intros.
-apply H.
-apply (TConst.WFact.union_iff C1 C2 _).
-left; trivial.
-Qed.
-
-Lemma Unified_empty : forall tsubst,
-  Unified TConst.empty tsubst.
-Proof.
-unfold Unified in |- *.
-intros.
-inversion H.
-Qed.
-
-Lemma Unified_Add_intro : forall c C tsubst,
-  Unified C tsubst ->
-  Unified (TConst.FSet.singleton c) tsubst ->
-  Unified (TConst.add c C) tsubst.
-Proof.
-unfold Unified in |- *.
-intros.
-apply (TConst.WFact.add_iff C c _) in H1.
-decompose [or] H1.
- destruct c.
- apply TConst.WFact.singleton_iff in H2.
- apply H0 in H2.
- trivial.
-
- apply H in H2.
- trivial.
-Qed.
-
-Lemma Unified_Add : forall c C tsubst,
-  Unified (TConst.add c C) tsubst -> Unified C tsubst.
-Proof.
-unfold Unified in |- *.
-intros.
-apply H.
-apply (TConst.WFact.add_iff C c _).
-right; trivial.
-Qed.
-
