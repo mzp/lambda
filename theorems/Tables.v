6c079f3a5162a1af221aad0eb68beb14871d07fc
mzp <mzpppp@gmail.com>
2010-01-17 11:17:38 +0900

    filter,unionをTableに移動した。

--- theorems/Tables.v
+++ theorems/Tables.v
@@ -2,6 +2,8 @@ Require Import String.
 Require Import FMapWeakList.
 Require Import FMapInterface.
 Require Import FMapFacts.
+
+Require Import Util.
 Require Import Dec.
 
 Module Table := FMapWeakList.Make StrDec.
@@ -137,4 +139,111 @@ unfold Table.In,Table.Raw.PX.In in |- *.
 unfold Table.MapsTo in H.
 exists T.
 tauto.
+Qed.
+
+Definition union {A : Type} (tsubst1 tsubst2 : table A) :=
+  Table.fold (fun key e m => Table.add key e m) tsubst1 tsubst2.
+
+Definition filter {A : Type} {P : string -> Prop} (dec : forall x, {P x} + {~ P x}) (tsubst : table A) :=
+  TableProp.filter
+    (fun key _ => if dec key then true else false)
+    tsubst.
+
+Lemma union_iff: forall A x (T : A) (X Y : table A),
+  Table.MapsTo x T (union X Y) <->
+  Table.MapsTo x T X \/ (~ Table.In x X /\ Table.MapsTo x T Y).
+Proof.
+intros.
+unfold union in |- *.
+pattern X,
+ (Table.fold
+    (fun (key : Table.key) (e : A) (m : Table.t A) => Table.add key e m) X Y)
+ in |- *.
+apply TableProp.fold_rec_bis; intros.
+ apply Extensionality_Table in H.
+ rewrite <- H in |- *.
+ tauto.
+
+ split; intros; auto.
+  right.
+  split; auto.
+  intro.
+  apply TableWF.empty_in_iff in H0.
+  contradiction.
+
+  decompose [or and] H; auto.
+  inversion H0.
+
+ split; intros.
+  apply TableWF.add_mapsto_iff in H2.
+  decompose [or and] H2.
+   left.
+   rewrite H4, H5 in |- *.
+   apply <- TableWF.add_mapsto_iff.
+   tauto.
+
+   apply H1 in H5.
+   decompose [or and] H5.
+    left.
+    apply <- TableWF.add_mapsto_iff.
+    tauto.
+
+    right.
+    split; auto.
+    Contrapositive H6.
+    apply TableWF.add_in_iff in H3.
+    decompose [or] H3; auto.
+    contradiction.
+
+  decompose [or and] H2.
+   apply TableWF.add_mapsto_iff in H3.
+   decompose [or and] H3.
+    rewrite H5, H6 in |- *.
+    apply <- TableWF.add_mapsto_iff.
+    tauto.
+
+    apply <- TableWF.add_mapsto_iff.
+    tauto.
+
+   apply <- TableWF.add_mapsto_iff.
+   right.
+   split.
+    Contrapositive H4.
+    apply <- TableWF.add_in_iff.
+    tauto.
+
+    apply H1.
+    right.
+    split; auto.
+    Contrapositive H4.
+    apply <- TableWF.add_in_iff.
+    tauto.
+Qed.
+
+Lemma filter_iff : forall A x P (dec : forall x, {P x} + {~ P x}) (T : A) (m : table A),
+  Table.MapsTo x T (filter dec m) <->
+  Table.MapsTo x T m /\ (P x).
+Proof.
+unfold filter in |- *.
+split; intros.
+ apply TableProp.filter_iff in H.
+  unfold Morphisms.Morphism,Morphisms.respectful in |- *.
+  intros.
+  rewrite H0 in |- *.
+  reflexivity.
+
+  decompose [and] H.
+  split; auto.
+  destruct (dec x); auto.
+  discriminate.
+
+ apply <- TableProp.filter_iff.
+  decompose [and] H.
+  split; auto.
+  destruct (dec x); auto.
+
+  unfold Morphisms.Morphism, Morphisms.respectful in |- *.
+  intros.
+  rewrite H0 in |- *.
+  reflexivity.
 Qed.
--- theorems/TypeSubst.v
+++ theorems/TypeSubst.v
@@ -32,14 +32,6 @@ Fixpoint term_subst t tsubst :=
      If (term_subst t1 tsubst) (term_subst t2 tsubst) (term_subst t3 tsubst)
   end.
 
-Definition union {A : Type} (tsubst1 tsubst2 : table A) :=
-  Table.fold (fun key e m => Table.add key e m) tsubst1 tsubst2.
-
-Definition filter {A : Type} {P : string -> Prop} (dec : forall x, {P x} + {~ P x}) (tsubst : table A) :=
-  TableProp.filter
-    (fun key _ => if dec key then true else false)
-    tsubst.
-
 Lemma mapsto_type_subst: forall x tsubst1 tsubst2,
  (forall U, Table.MapsTo x U tsubst1 <-> Table.MapsTo x U tsubst2) ->
  type_subst (VarT x) tsubst1 = type_subst (VarT x) tsubst2.
@@ -67,106 +59,6 @@ destruct (TableWF.In_dec tsubst1 x).
   rewrite n,H0 in |- *.
   reflexivity.
 Qed.
-
-Lemma union_iff: forall A x (T : A) (X Y : table A),
-  Table.MapsTo x T (union X Y) <->
-  Table.MapsTo x T X \/ (~ Table.In x X /\ Table.MapsTo x T Y).
-Proof.
-intros.
-unfold union in |- *.
-pattern X,
- (Table.fold
-    (fun (key : Table.key) (e : A) (m : Table.t A) => Table.add key e m) X Y)
- in |- *.
-apply TableProp.fold_rec_bis; intros.
- apply Extensionality_Table in H.
- rewrite <- H in |- *.
- tauto.
-
- split; intros; auto.
-  right.
-  split; auto.
-  intro.
-  apply TableWF.empty_in_iff in H0.
-  contradiction.
-
-  decompose [or and] H; auto.
-  inversion H0.
-
- split; intros.
-  apply TableWF.add_mapsto_iff in H2.
-  decompose [or and] H2.
-   left.
-   rewrite H4, H5 in |- *.
-   apply <- TableWF.add_mapsto_iff.
-   tauto.
-
-   apply H1 in H5.
-   decompose [or and] H5.
-    left.
-    apply <- TableWF.add_mapsto_iff.
-    tauto.
-
-    right.
-    split; auto.
-    Contrapositive H6.
-    apply TableWF.add_in_iff in H3.
-    decompose [or] H3; auto.
-    contradiction.
-
-  decompose [or and] H2.
-   apply TableWF.add_mapsto_iff in H3.
-   decompose [or and] H3.
-    rewrite H5, H6 in |- *.
-    apply <- TableWF.add_mapsto_iff.
-    tauto.
-
-    apply <- TableWF.add_mapsto_iff.
-    tauto.
-
-   apply <- TableWF.add_mapsto_iff.
-   right.
-   split.
-    Contrapositive H4.
-    apply <- TableWF.add_in_iff.
-    tauto.
-
-    apply H1.
-    right.
-    split; auto.
-    Contrapositive H4.
-    apply <- TableWF.add_in_iff.
-    tauto.
-Qed.
-
-Lemma filter_iff : forall A x P (dec : forall x, {P x} + {~ P x}) (T : A) (m : table A),
-  Table.MapsTo x T (filter dec m) <->
-  Table.MapsTo x T m /\ (P x).
-Proof.
-unfold filter in |- *.
-split; intros.
- apply TableProp.filter_iff in H.
-  unfold Morphisms.Morphism,Morphisms.respectful in |- *.
-  intros.
-  rewrite H0 in |- *.
-  reflexivity.
-
-  decompose [and] H.
-  split; auto.
-  destruct (dec x); auto.
-  discriminate.
-
- apply <- TableProp.filter_iff.
-  decompose [and] H.
-  split; auto.
-  destruct (dec x); auto.
-
-  unfold Morphisms.Morphism, Morphisms.respectful in |- *.
-  intros.
-  rewrite H0 in |- *.
-  reflexivity.
-Qed.
-
 
 
 (*Lemma add_eq : forall x T tenv tsubst,
